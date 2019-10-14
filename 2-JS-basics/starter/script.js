bc0573a7de9d570df01bad20d9cc9835c2ff792c
Accio26 <acc1iela@gmail.com>
2019-10-15 02:23:28 +0900

    2-JS-basics 基礎

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,17 @@
+const firstName = 'John';
+console.log(firstName);
+
+const lastName = 'Smith';
+const age = 28;
+
+const fullAge = true;
+console.log(fullAge);
+
+let job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+let e3years = 3;
+let johnMark = 'John and Mark';
