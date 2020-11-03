41a628694b98afe2ead2dbe57128c01789b202f2
acc1iela <acc1iela@gmail.com>
2020-11-04 02:32:13 +0900

    課題4終了

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -29,7 +29,7 @@ let continent = 'Eurasia';
 let population = '750,000';
 
 console.log(country, continent, population);
-*/
+
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -60,3 +60,56 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991; // 再代入も出来ないよ
+// birthYear = 1190;
+
+// const job; 初期化式がないからエラーが走る
+
+var job = 'programmer';
+job = 'teacher';
+
+
+// test
+const language = 'Japanese';
+*/
+
+const now = 2037;
+
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+
+// Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // 46 > 19
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
+
+
+// test
+let population = 750000 / 2;
+population++;
+console.log(population);
+console.log(6000000 < population);
+console.log(33000000 > population);
