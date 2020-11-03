579a013fad2c0f4180f3d619d59248d0604b1e62
acc1iela <acc1iela@gmail.com>
2020-11-04 01:36:14 +0900

    01-foundamentals first 課題終了

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,30 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log('23');
+
+let firstName = 'Jonas';
+let first = 'Matilda';
+let firstNamePerson;
+
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+let PI = 3.1415;
+
+// better
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+// not_pattern
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+// test
+let country = 'Japan';
+let continent = 'Eurasia';
+let population = '750,000';
+
+console.log(country, continent, population);
