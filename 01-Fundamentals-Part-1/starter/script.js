ef426c4be83774489fdb0cdf8299345bee864359
acc1iela <acc1iela@gmail.com>
2020-11-04 02:01:34 +0900

    next work2 終了

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -28,3 +29,34 @@ let continent = 'Eurasia';
 let population = '750,000';
 
 console.log(country, continent, population);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true); // boolean
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+console.log(typeof 'jonas');
+
+javascriptIsFun = 'YES!';
+
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year); // undefined
+console.log(typeof year); // undefined
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); // object 歴史的経緯のあるバグ 本来は当然nullになる
+
+// test
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
