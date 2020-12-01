ac42777844a8a5a8f557b7d2d21bce575aba775f
acc1iela <acc1iela@gmail.com>
2020-12-01 23:50:36 +0900

    section2 終了

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,68 @@
+'use strict'
+
+/* section 1
+let hasDriverLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) console.log('I can drive :D');
+*/
+
+/*
+function logger() {
+  console.log('My name is Jonas');
+}
+
+// es6
+const loggers = (() => {
+  console.log('apple');
+})
+
+logger();
+loggers();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+fruitProcessor('penis', 'index');
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
+/*
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+}
+*/
+
+
+/*
+// arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = birthYear => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+}
+
+*/
+
+
+
+// section2だいぶ飛ばした
