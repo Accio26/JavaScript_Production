6833797ab3b82574ad9f2d53fd72ac845fcf257b
acc1iela <acc1iela@gmail.com>
2021-01-03 23:32:21 +0900

    関数スコープセクション 終了

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName},you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1998) {
+      const firstName = 'Steaven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      output = 'new output!';
+    }
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Accio';
+calcAge(1997);
