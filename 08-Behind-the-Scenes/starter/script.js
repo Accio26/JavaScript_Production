ece092e8255540b5322747defba97f2a35d73e61
acc1iela <acc1iela@gmail.com>
2021-01-05 22:49:55 +0900

    behind section

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,27 +1,41 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `${firstName},you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName},you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1998) {
-      const firstName = 'Steaven';
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 1998) {
+//       const firstName = 'Steaven';
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-      // output = 'new output!';
-    }
-    console.log(output);
-  }
-  printAge();
-  return age;
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       // output = 'new output!';
+//     }
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Accio';
+// calcAge(1997);
+
+var me = 'Accio';
+let job = 'teacher';
+const year = 1991;
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Accio';
-calcAge(1997);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
